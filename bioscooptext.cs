038c7bcb6722917e2de3ecb165ef782e4e9c8915
DavidGiesenbot <1015843@hr.nl>
2021-04-06 14:22:06 +0200

    toegevoegd: bioscooptext(leeg) Catalog(json file met films) film_display(laat de film info zien)

---
+++ bioscooptext.cs
@@ -0,0 +1,5 @@
+class Program{
+    public static void Main(string[] args){
+        Console.Writeline("test");
+    }
+}
---
+++ film_display.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Team_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
