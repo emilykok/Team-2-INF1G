9ac035e031a25b6291578b0b961445d253b49450
MelissaKros <1019740@hr.nl>
2021-04-06 14:22:47 +0200

    jsons voor eten en drinken + begin code menu

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace HelloWorld
+{
+  class Program {
+    public static void Main() {
+        Console.WriteLine("Typ het nummer van wat je wilt bekijken en klink op enter\n1. Popcorn zoet\n2. Popcorn zout\n3. Popcorn karamel\n4. M&M's pinda\n5. M&M's chocola\n6. Chips naturel\n7. Chips paprika\n8. Doritos nacho cheese\n9. Haribo goudberen\n10. Skittles fruits");
+        int foodItem = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(foodItem);
+    }
+  }
+}   
