760909df1291e7248cd32d1bfc467a098e02b2a7
MelissaKros <1019740@hr.nl>
2021-04-20 21:39:38 +0200

    json verwerking werkend
    pricipe systeem gemaakt voor displaying

--- Team-2-INF1G/Program.cs
+++ Team-2-INF1G/Program.cs
@@ -1,16 +1,67 @@
-﻿
+﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml;
+using Formatting = Newtonsoft.Json.Formatting;
+using JsonIgnoreAttribute = Newtonsoft.Json.JsonIgnoreAttribute;
 
 namespace Project
 {
     public class Eten
     {
+        // declaring struct
+        public struct EtenData {
+            public string naam;
+            public string[] inhoud;
+            public double[] prijs;
+            public string voedingswaarde;
+            public string[] allergenen;
+            public string[] tags;
+        }
+        // creates a list of type EtenData
+        public List<EtenData> etenDataList = new List<EtenData>();        
+        
+        [JsonIgnore]
+        public string jsonPath;
+        [JsonIgnore]
+        public string path;
+
+        // Constructor
+        public Eten() {
+            // Start van path vinden //
+            this.path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\Foods.json"));
+            this.jsonPath = File.ReadAllText(path);
+            this.etenDataList = JsonConvert.DeserializeObject<List<EtenData>>(jsonPath);
+            // End van path vinden //
+        }
+
+        public string ToJSON(){
+            return JsonConvert.SerializeObject(this.etenDataList, Formatting.Indented);
+        }
+        
         // menu //
-        public int etemMenu()
+        public void EtenMenu()
         {
-            Console.WriteLine("Typ het nummer van wat je wilt bekijken en klink op enter\n1. Popcorn zoet - va 2,99\n2. Popcorn zout - va 2,49\n3. Popcorn karamel - va 2,49\n4. M&M's pinda - 3,99\n5. M&M's chocola - 4,49\n6. Chips naturel\n7. Chips paprika\n8. Doritos nacho cheese\n9. Haribo goudberen\n10. Skittles fruits");
-            int num = Convert.ToInt32(Console.ReadLine());
-            return num;
+            Console.WriteLine("1. Popcorn zoet - va 2,99\n2. Popcorn zout - va 2,49\n3. Popcorn karamel - va 2,49\n4. M&M's pinda - 3,99\n5. M&M's chocola - 4,49\n6. Chips naturel - va 2,99\n7. Chips paprika - va 2,99\n8. Doritos nacho cheese - 3,99\n9. Haribo goudberen - 3,49\n10. Skittles fruits - 3,99");
+            Console.WriteLine("\nTyp het nummer van wat je wilt bekijken en klik op enter:");
+            
+            string input = Console.ReadLine();
+            int num = Convert.ToInt32(input);
+            Console.Clear();
+            
+            Console.WriteLine($"naam: {etenDataList[num-1].naam}\ninhoud: {etenDataList[num-1].inhoud}\n.....");
+            Console.WriteLine("Typ 'x' om terug te gaan of '<' om een nieuwe item te selecteren");
+
+            input = Console.ReadLine();
+            if (input == "x" || input == "X") { };
+            if (input == "<") EtenMenu();
+            
+
+            /*  if (num == 1) Console.WriteLine("naam: Popcorn zoet \ninhoud: 85g, 180g, 325g \nprijs: 2.99, 4.99, 6.99 \nvoedingswaarde per 100gr:\n   Energie: 1794kj(424kcal)\n   Vet: 5,2g\n   Waarvan verzadigd: 0,7g\n   Koolhydraten: 89g\n   Waarvan suikers: 55g\n   Voedingsvezel: 3,6g\n   Eiwitten: 3,5g\n   Zout: 0g \nAllergenen: veganistisch, glutenvrij, \ntags: zoet");
+            else Console.WriteLine($"Item nummer {num} is nog niet toegevoegd.");*/
         }
 
         // food info //
@@ -25,90 +76,14 @@ namespace Project
         //if (Console.ReadLine() == 'x') call menu()
         //}
     }
-}   
-  }    
-
-/*
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Xml;
-using Formatting = Newtonsoft.Json.Formatting;
-using JsonIgnoreAttribute = Newtonsoft.Json.JsonIgnoreAttribute;
-
-namespace Team_2
-{
-
-    public class Account
-    {
-        public string User_Name { get; set; }
-        public string Password { get; set; }
-
-        [JsonIgnore] // Dit ignored de value voor als je naar JSON ombouwt
-        public string path;
-        [JsonIgnore]
-        public string json_path;
-
-        public string ToJSON()
-        {
-            return JsonConvert.SerializeObject(this, Formatting.Indented);
-        }
-
-        public Account FromJSON(string json_path) // FromJSON returned een Account format, dus de field
-        {
-            return JsonConvert.DeserializeObject<Account>(json_path); ;
-        }
-
-        public Account()
-        {
-            // Start van path vinden //
-            this.path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\Test\Accounts.json"));
-            this.json_path = File.ReadAllText(this.path);
-            // End van path vinden //
-        }
-
-
-        public void Create()
-        {
-            bool retry = true;
-
-            // User input username and password, retry while statement if user wants to retry //
-            while (retry == true)
-            {
-                Console.WriteLine("\nVoer een gebruikersnaam in: ");
-                this.User_Name = Console.ReadLine();
-                Console.WriteLine("\nVoer een wachtwoord in: ");
-                this.Password = Console.ReadLine();
-                Console.WriteLine($"\nGeselecteerde gebruikersnaam: {User_Name} | Geselecteerde wachtwoord: {Password} \nOm te confirmeren toets ENTER, anders toets 'X'");
-
-                // Checked if user wants to retry or confirm username //
-                string confirm = Console.ReadLine();
-                if (confirm == "X" || confirm == "x" || confirm == "'X'" || confirm == "'x'")
-                {
-                    retry = true;
-                }
-                else
-                {
-                    retry = false;
-                }
-            }
-            // Write naar JSON file
-            System.IO.File.WriteAllText(this.path, ToJSON());
-            Console.WriteLine($"Account gecreeerd | Gebruikersnaam: {User_Name} | Wachtwoord: {Password}");
-        }
-    }
     class Program
     {
-
-
         static void Main(string[] args)
         {
 
-            new Account().Create();
+            Eten eten = new Eten();
+            eten.EtenMenu();
 
         }
     }
-} */
+}    
