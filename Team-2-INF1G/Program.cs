bc39dade9776cf310ea153cf5c9f76c0f9b69055
DavidGiesenbot <1015843@hr.nl>
2021-04-20 21:42:46 +0200

    lijst aan titels
    lijst gemaakt om uit te kiezen

--- Team-2-INF1G/Program.cs
+++ Team-2-INF1G/Program.cs
@@ -1,31 +1,18 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Xml;
-using Formatting = Newtonsoft.Json.Formatting;
-using JsonIgnoreAttribute = Newtonsoft.Json.JsonIgnoreAttribute;
+﻿using System;
 
 namespace CinaMatrix
 {
     public class MovieDetail
     {
         // film informatie uit de json file halen(WIP) //
-        public string Titel { get; set; }
-        public double rating { get; set; }
-        public Tuple<int, string[]> kijkwijzer { get; set; }
-        public string[] genre { get; set; }
-        public string regisseur { get; set; }
-        public int speeltijd { get; set; }
-        public string[] acteurs { get; set; }
-        public string samenvatting { get; set; }
-
-        [JsonIgnore] // Dit ignored de value voor als je naar JSON ombouwt
-        public string path;
-        [JsonIgnore]
-        public string json_path;
+        public string Titel;
+        public double rating;
+        public Tuple<int, string[]> kijkwijzer;
+        public string[] genre;
+        public string regisseur;
+        public int speeltijd;
+        public string[] acteurs;
+        public string samenvatting;
 
         public MovieDetail()
         {
@@ -39,23 +26,27 @@ namespace CinaMatrix
             this.samenvatting = "De twintiger Lucy werkt in een kunstgalerie en is op persoonlijk vlak een grote verzamelaar.\nAls ze door haar vriendje gedumpt wordt, krijgt ze het idee om het project The Broken Heart Gallery op te zetten,\neen verzamelplek voor alle objecten die te maken hebben met haar vroegere liefdes. De expositie wordt een succes en\nkrijgt navolging.";
         }
 
-        public Account FromJSON(string json_path) // FromJSON returned een Account format, dus de field
+        public static string[] GetList(int page)
         {
-            return JsonConvert.DeserializeObject<Account>(json_path); ;
-        }
-
-        public Account()
-        {
-            // Start van path vinden //
-            this.path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\Accounts.json"));
-            this.json_path = File.ReadAllText(this.path);
-            // End van path vinden //
+            int index;
+            if (page == 1) index = 1;
+            else index = 1 + (10 * (page - 1));
+            string[] titleList;
+            if (index == 1)
+            {
+                titleList = new string[10] { "The Broken Hearts Gallery", "Words On The Bathroom Walls", "Valley Girl", "The Half of It", "The Lovebirds", "Happiest Season", "Titanic", "Dirty Dancing", "The Kissing Booth 2", "Deadpool" };
+            }
+            else
+            {
+                titleList = new string[10] { "Men in Black: International", "Godzilla: King of the Monsters", "Captain Marvel", "X-Men: Dark Phoenix", "Star Wars: The Rise of Skywalker", "Io", "Avengers: Endgame", "Code 8", "I Am Mother", "Ad Astra" };
+            }
+            return titleList;
         }
     }
 
     class Program
     {
-        public static string arrToString(string[] before)
+        public static string ArrToString(string[] before)
         {
             // functie om van string array een enkele string te maken //
             string s = "";
@@ -67,22 +58,64 @@ namespace CinaMatrix
             return s;
         }
 
-        public static void displayMovie()
+        public static void DisplayMovie()
         {
             // laat alle informatie van de gekozen film zien //
             MovieDetail a = new MovieDetail();
             Console.WriteLine($"Titel: {a.Titel}\n");
-            Console.WriteLine($"Genre: {arrToString(a.genre)}\nKijkwijzer: {a.kijkwijzer.Item1}, {arrToString(a.kijkwijzer.Item2)}\n");
-            Console.WriteLine($"Regisseur: {a.regisseur}\nActeurs: {arrToString(a.acteurs)}\n");
+            Console.WriteLine($"Genre: {ArrToString(a.genre)}\nKijkwijzer: {a.kijkwijzer.Item1}, {ArrToString(a.kijkwijzer.Item2)}\n");
+            Console.WriteLine($"Regisseur: {a.regisseur}\nActeurs: {ArrToString(a.acteurs)}\n");
             Console.WriteLine($"samenvatting: {a.samenvatting}\n");
             Console.WriteLine($"Rating: {a.rating}\nSpeeltijd: {a.speeltijd} minuten");
         }
+
+        public static void MovieList(int page)
+        {
+            // laat een lijst aan films zien //
+            string[] titleList = MovieDetail.GetList(page);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i + 1}.  {titleList[i]}");
+            }
+        }
         static void Main(string[] args)
         {
-            displayMovie();
-            Console.WriteLine("type a number");
-            int el = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"input was: {el}");
+            int page = 1;
+            MovieList(page);
+            while (true)
+            {
+                var navigation = Console.ReadLine();
+                if (navigation == "<" || navigation == ">")
+                {
+                    if (navigation == "<" && page > 1)
+                    {
+                        Console.Clear();
+                        page--;
+                        MovieList(page);
+                    }
+                    else if (navigation == ">" && page < 2)
+                    {
+                        Console.Clear();
+                        page++;
+                        MovieList(page);
+                    }
+                }
+                else
+                {
+                    // input om proberen te zetten naar int //
+                    try
+                    {
+                        int selection = Convert.ToInt32(navigation);
+                        Console.Clear();
+                        if (selection == 1) DisplayMovie();
+                        else Console.WriteLine("deze film is nog niet beschikbaar");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("dit is geen geldige invulwaarde");
+                    }
+                }
+            }
         }
     }
 }
