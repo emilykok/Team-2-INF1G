d834d66808fd0011dcd671f4c0d8114c3a234851
bjornkok <1007695@hr.nl>
2021-04-21 15:43:21 +0200

    Code samenvoeging #1
    Al onze bestanden met elkaar samengevoegd, en wat links gelegd

---
+++ Team-2-INF1G/Team-2-INF1G/Classes/Eten_Class.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml;
+using Formatting = Newtonsoft.Json.Formatting;
+using JsonIgnoreAttribute = Newtonsoft.Json.JsonIgnoreAttribute;
+
+namespace Eten_Class
+{
+    public class Eten
+    {
+        // declaring struct for json data
+        public struct EtenData
+        {
+            public string naam;
+            public string[] inhoud;
+            public double[] prijs;
+            public string voedingswaarde;
+            public string[] allergenen;
+            public string[] tags;
+            public int clicks;
+        }
+
+        // creates a list of type EtenData
+        public List<EtenData> etenDataList = new List<EtenData>();
+
+        [JsonIgnore]
+        public string jsonPath;
+        [JsonIgnore]
+        public string path;
+
+        // Constructor
+        public Eten()
+        {
+            this.path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\Foods.json"));
+            this.jsonPath = File.ReadAllText(path);
+            this.etenDataList = JsonConvert.DeserializeObject<List<EtenData>>(jsonPath);
+        }
+
+        public string ToJSON()
+        {
+            return JsonConvert.SerializeObject(this.etenDataList, Formatting.Indented);
+        }
+
+        // fooddisplay
+        public void EtenMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Eten Menu:\n1. Popcorn zoet - \t\tva 2,99\n2. Popcorn zout - \t\tva 2,49\n3. Popcorn karamel - \t\tva 2,49\n4. M&M's pinda - \t\t3,99\n5. M&M's chocola - \t\t4,49\n6. Chips naturel - \t\tva 2,99\n7. Chips paprika - \t\tva 2,99\n8. Doritos nacho cheese - \t3,99\n9. Haribo goudberen - \t\t3,49\n10. Skittles fruits - \t\t3,99");
+            Console.WriteLine("\nTyp het nummer van de item die je wilt bekijken en klik op enter:");
+
+            // leest input command van de console
+            string input = Console.ReadLine();
+            Console.Clear();
+
+            try // in t geval dat de input te hoog is of niet convertible is tot int
+            {
+                int num = Convert.ToInt32(input);
+                // print naam item
+                Console.WriteLine($"{etenDataList[num - 1].naam}\n");
+
+                // print inhoud item
+                string inh = "";
+                for (int i = 0; i < (etenDataList[num - 1].inhoud).Length; i++)
+                {
+                    inh += etenDataList[num - 1].inhoud[i] + ", ";
+                }
+
+                Console.WriteLine("inhoud: " + inh);
+                // print prijs item
+                string pri = "";
+                for (int i = 0; i < (etenDataList[num - 1].prijs).Length; i++)
+                {
+                    pri += etenDataList[num - 1].prijs[i] + ", ";
+                }
+                Console.WriteLine("prijs: " + pri + "\n");
+
+                // print voedingswaarde
+                Console.WriteLine($"{etenDataList[num - 1].voedingswaarde}\n");
+
+                // print allergenen
+                string al = "";
+                for (int i = 0; i < (etenDataList[num - 1].allergenen).Length; i++)
+                {
+                    al += etenDataList[num - 1].allergenen[i] + ", ";
+                }
+                Console.WriteLine("allergenen: " + al);
+
+                // input na de display
+                bool goodInput = false;
+                while (!goodInput)
+                {
+                    Console.WriteLine("\nTyp 'x' om terug te gaan of '>' om een andere item te selecteren.");
+                    input = Console.ReadLine();
+                    Console.Clear();
+                    if (input == "x" || input == "'x'" || input == "X" || input == "'X'") { goodInput = true; break; }
+                    else if (input == ">" || input == "'>'") { EtenMenu(); goodInput = true; break; }
+                    else Console.WriteLine("Er is een onjuist command ingevuld, probeer het nog eens.");
+                }
+            }
+
+            catch (Exception e)
+            { // input is niet convertible naar int of input is te hoog nummer
+                Console.Clear();
+                Console.WriteLine("De input is niet juist, probeer het nogeens\n");
+                EtenMenu();
+            }
+            Console.Clear();
+
+        }
+
+        /* public void UpdateClick(int n)
+         {
+             int currClick = etenDataList[n - 1].clicks;
+             currClick++;
+             etenDataList[n - 1].clicks = currClick;
+
+             // write to the JSON file (updates the file)
+             System.IO.File.WriteAllText(this.path, ToJSON());
+         }*/
+    }
+} 
--- Team-2-INF1G/Team-2-INF1G/Program.cs
+++ Team-2-INF1G/Team-2-INF1G/Program.cs
@@ -8,232 +8,43 @@ using System.Xml;
 using Formatting = Newtonsoft.Json.Formatting;
 using JsonIgnoreAttribute = Newtonsoft.Json.JsonIgnoreAttribute;
 
+using Account_Class;
+using Eten_Class;
+using MovieDetail_Class;
+using Ticket_Class;
+using Hoofdmenu;
+
 namespace Team_2
 {
-    public class Account
-    {
-        //// struct is a data type used for the serialization and deserialization of the JSON file.
-        // It will be put in a list, that can be index with []
-        // To call the value's in it, use the name and then the value name (accountDataList[0].Age)
-        // It is the same as a Tuple, but it works like a class as reference (acc.accountDataList[0].Age)
-        // You can also use a foreach to loop through all the items in the list.
-            //foreach (var accountData in accountDataList)
-            //{
-            //Console.WriteLine(accountData.Password);
-            //}
-
-        public struct AccountData
-        {
-            public string Name;
-            public string Password;
-            
-            public int Age;
-            public string Gender;
-            public string Email;
-            public string bankingDetails;
-            public string[] Allergies;
-        }
-
-        //// Field
-        // Creates a list, a data type that can be added upon, with the "AccountData" type
-        public List<AccountData> accountDataList = new List<AccountData>();
-
-        [JsonIgnore]
-        public string jsonPath;
-        [JsonIgnore]
-        public string path; 
-
-        // Constructor
-        public Account()
-        {
-            // this.path is used in serializing the json data.
-            this.path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\Accounts.json"));
-            // this.jsonPath is used in deserializing the json data.
-            this.jsonPath = File.ReadAllText(path);
-            // the constructor loads the json file first, so it can be modified later in the file.
-            this.accountDataList = JsonConvert.DeserializeObject<List<AccountData>>(jsonPath);
-
-        }
-
-        // Method that is used to write data to the JSON file.
-        public string ToJSON()
-        {
-            return JsonConvert.SerializeObject(this.accountDataList, Formatting.Indented);
-        }
-
-        // Method to check if username is already taken, returns bool (true if taken, false if not taken)
-        public bool UsernameCheck(string username)
-        {
-            foreach (var accountData in accountDataList)
-            {
-                if(username == accountData.Name)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-
-        // Method to login, returns int (the index of the list which corresponds to user selected) if user is found, else returns -1
-        public int Login(string name, string password)
-        {
-            for (int i = 0; i < accountDataList.Count; i++)
-            {
-                if (name == accountDataList[i].Name && password == accountDataList[i].Password)
-                {
-                    return i;
-                }
-            }
-            return -1;
-        }
-
-        // Method that returns user account index, based on input => calls login method.
-        public int TextLogin()
-        {
-            // retry bool for if the user wants to try again
-            bool retry = true;
-            bool failed = false;
-
-            while (retry == true)
-            {
-                // Clears the console for typing;
-                Console.Clear();
-
-                // Ask for user input
-                Console.WriteLine("*---------------*");
-                if (failed == true)
-                {
-                    Console.WriteLine("Gebruikersnaam of wachtwoord was incorrect, probeer het opnieuw\n");
-                }
-                Console.WriteLine("Voer een gebruikersnaam in: ");
-                string User_Name = Console.ReadLine();
-                Console.WriteLine("\nVoer een wachtwoord in: ");
-                string Password = Console.ReadLine();
-                Console.WriteLine($"\nGeselecteerde gebruikersnaam: {User_Name} | Geselecteerde wachtwoord: {Password} \nOm in te loggen toets ENTER\nOm opniew te proberen, toets 'r'\nOm terug te gaan, toets 'x'");
-
-                // Checked if user wants to retry or confirm username //
-                string confirm = Console.ReadLine();
-                if (confirm == "R" || confirm == "r" || confirm == "'R'" || confirm == "'r'")
-                {
-                    retry = true;
-                }
-
-                else if (confirm == "X" || confirm == "x" || confirm == "'X'" || confirm == "'x'")
-                {
-                    retry = false;
-                }
-                else
-                {
-                    retry = false;
-                    int login_Result = this.Login(User_Name, Password);
-                    if (login_Result == -1)
-                    {
-                        failed = true;
-                        retry = true;
-                    }
-                    else
-                    {
-                        return login_Result;
-                    }
-                }
-            }
-            return -1;
-        }
-
-
-        // Method that can be called to create a user.
-        public void CreateUser(string name, string password)
-        {
-            
-            AccountData newAccountData = new AccountData();
-            newAccountData.Name        = name;
-            newAccountData.Password    = password;
-            
-            // placeHolders
-            newAccountData.Age              = -1;
-            newAccountData.Gender           = null;
-            newAccountData.Email            = null;
-            newAccountData.bankingDetails   = null;
-            newAccountData.Allergies        = null;
-            // add to the list with the added data
-            accountDataList.Add(newAccountData);
-
-            // write to the JSON file (updates the file)
-            System.IO.File.WriteAllText(this.path, ToJSON());
-        }
-        
-        // Method that creates user, based on input => calls CreateUser method.
-        public void TextCreateUser()
-        {
-            // retry bool for if the user wants to try again
-            bool retry = true;
-            
-            while (retry == true){
-                // Clears the console for typing;
-                Console.Clear();
-
-                // Ask for user input
-                Console.WriteLine("*---------------*");
-                Console.WriteLine("Voer een gebruikersnaam in: ");
-                string User_Name = Console.ReadLine();
-
-                bool recheck = true;
-                if (UsernameCheck(User_Name) == true && recheck == true)
-                {
-                    Console.WriteLine("\nDeze gebruikersnaam bestaat al, probeer opnieuw: ");
-                    User_Name = Console.ReadLine();
-                    recheck = UsernameCheck(User_Name);
-                }
-
-                Console.WriteLine("\nVoer een wachtwoord in: ");
-                string Password = Console.ReadLine();
-                Console.WriteLine($"\nGeselecteerde gebruikersnaam: {User_Name} | Geselecteerde wachtwoord: {Password} \nOm te confirmeren toets ENTER\nOm opniew te proberen, toets 'r'\nOm terug te gaan, toets 'x'");
-                
-                // Checked if user wants to retry or confirm username //
-                string confirm = Console.ReadLine();
-                if (confirm == "R" || confirm == "r" || confirm == "'R'" || confirm == "'r'")
-                {
-                    retry = true;
-                }
-                else if (confirm == "X" || confirm == "x" || confirm == "'X'" || confirm == "'x'")
-                {
-                    retry = false;
-                }
-                else
-                {
-                    retry = false;
-                    this.CreateUser(User_Name, Password);
-                }
-            }
-        }
-
-
-        // Method that updates selective user, requires to be logged in before or index!
-        // Also requires an int for the item. The following ints represent (1 == )
-        public void UpdateUser(int item, int index)
-        {
-            //placeholder
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             
+            // BJORN GEDEELTE //
             Account acc = new Account();
 
+            // Creates user in JSON
+            acc.TextCreateUser();
+
+            // Login with username and password
             int print_this = acc.TextLogin();
             Console.WriteLine(print_this);
 
-            //// Update account
-            // inlog index value
+            // MELISSA GEDEELTE //
+            Eten eten = new Eten();
+            eten.EtenMenu();
 
-            // Remove account
+            // DAVID GEDEELTE //
+            MovieDetail.CodeActivate();
 
+            // NOAH GEDEELTE //
+            reservering resv = new reservering();
+            resv.RunTickets();
 
-
+            // JAMIE GEDEELTE //
+            Applicatie myApllicatie = new Applicatie();
+            myApllicatie.Start();
         }
     }
 }
