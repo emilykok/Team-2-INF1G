67253e9d4afbe48c468c1b6427a1cca94e0ebb2d
DavidGiesenbot <1015843@hr.nl>
2021-04-21 15:50:04 +0200

    werkende lijst
    werkende lijst

--- Team-2-INF1G/Program.cs
+++ Team-2-INF1G/Program.cs
@@ -1,91 +1,131 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Formatting = Newtonsoft.Json.Formatting;
+using JsonIgnoreAttribute = Newtonsoft.Json.JsonIgnoreAttribute;
 
-namespace CinaMatrix
+namespace Team_2_INF1G
 {
     public class MovieDetail
     {
         // film informatie uit de json file halen(WIP) //
-        public string Titel;
-        public double rating;
-        public Tuple<int, string[]> kijkwijzer;
-        public string[] genre;
-        public string regisseur;
-        public int speeltijd;
-        public string[] acteurs;
-        public string samenvatting;
+        public struct MovieData
+        {
+            public string titel;
+            public double rating;
+            public string[] kijkwijzer;
+            public string[] genre;
+            public string regisseur;
+            public int speeltijd;
+            public string[] acteurs;
+            public string samenvatting;
+        }
+
+        public List<MovieData> movieDataList = new List<MovieData>();
+
+        [JsonIgnore]
+        public string jsonPath;
+        [JsonIgnore]
+        public string path;
 
         public MovieDetail()
         {
-            this.Titel = "The Broken Hearts Gallery";
-            this.rating = 6.2;
-            this.kijkwijzer = Tuple.Create(12, new string[] { "seks", "grof taalgebruik" });
-            this.genre = new string[] { "romantiek", "komedie" };
-            this.regisseur = "Natalie Krinsky";
-            this.speeltijd = 108;
-            this.acteurs = new string[] { "Geraldine Viswanathan", "Dacre Montgomery", "Utkarsh Ambudkar" };
-            this.samenvatting = "De twintiger Lucy werkt in een kunstgalerie en is op persoonlijk vlak een grote verzamelaar.\nAls ze door haar vriendje gedumpt wordt, krijgt ze het idee om het project The Broken Heart Gallery op te zetten,\neen verzamelplek voor alle objecten die te maken hebben met haar vroegere liefdes. De expositie wordt een succes en\nkrijgt navolging.";
+            path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\Catalog.json"));
+            jsonPath = File.ReadAllText(path);
+            movieDataList = JsonConvert.DeserializeObject<List<MovieData>>(jsonPath);
         }
 
+        public string ToJSON()
+        {
+            return JsonConvert.SerializeObject(movieDataList, Formatting.Indented);
+        }
+        /*
         public static string[] GetList(int page)
         {
             int index;
             if (page == 1) index = 1;
             else index = 1 + (10 * (page - 1));
             string[] titleList;
-            if (index == 1)
-            {
-                titleList = new string[10] { "The Broken Hearts Gallery", "Words On The Bathroom Walls", "Valley Girl", "The Half of It", "The Lovebirds", "Happiest Season", "Titanic", "Dirty Dancing", "The Kissing Booth 2", "Deadpool" };
-            }
-            else
-            {
-                titleList = new string[10] { "Men in Black: International", "Godzilla: King of the Monsters", "Captain Marvel", "X-Men: Dark Phoenix", "Star Wars: The Rise of Skywalker", "Io", "Avengers: Endgame", "Code 8", "I Am Mother", "Ad Astra" };
-            }
-            return titleList;
+            //for(int i = 0, x = index; i < 10; i++, x++)
+            //{
+            //    titleList
+            //}
+            //return titleList;
         }
-    }
+        */
+        /*
+        public void CreateMovie(string Titel, double Rating, string[] Kijkwijzer, string[] Genre, string Regisseur, int Speeltijd, string[] Acteurs, string Samenvatting)
+        {
+            MovieData newMovieData = new MovieData();
+            newMovieData.titel = Titel;
+            newMovieData.rating = Rating;
+            newMovieData.kijkwijzer = Kijkwijzer;
+            newMovieData.genre = Genre;
+            newMovieData.regisseur = Regisseur;
+            newMovieData.speeltijd = Speeltijd;
+            newMovieData.acteurs = Acteurs;
+            newMovieData.samenvatting = Samenvatting;
 
-    class Program
-    {
+            movieDataList.Add(newMovieData);
+            Console.WriteLine(movieDataList);
+            System.IO.File.WriteAllText(this.path, ToJSON());
+        }
+        */
+
         public static string ArrToString(string[] before)
         {
             // functie om van string array een enkele string te maken //
             string s = "";
-            for (int i = 0; i < (before.Length - 1); i++)
+            for (int i = 0; i < before.Length - 1; i++)
             {
                 s += before[i] + ", ";
             }
-            s += before[(before.Length - 1)];
+            s += before[before.Length - 1];
             return s;
         }
 
-        public static void DisplayMovie()
+        public static void DisplayMovie(int index)
         {
             // laat alle informatie van de gekozen film zien //
-            MovieDetail a = new MovieDetail();
-            Console.WriteLine($"Titel: {a.Titel}\n");
-            Console.WriteLine($"Genre: {ArrToString(a.genre)}\nKijkwijzer: {a.kijkwijzer.Item1}, {ArrToString(a.kijkwijzer.Item2)}\n");
-            Console.WriteLine($"Regisseur: {a.regisseur}\nActeurs: {ArrToString(a.acteurs)}\n");
-            Console.WriteLine($"samenvatting: {a.samenvatting}\n");
-            Console.WriteLine($"Rating: {a.rating}\nSpeeltijd: {a.speeltijd} minuten");
+            MovieDetail movie = new MovieDetail();
+            Console.WriteLine("vul < in om terug te gaan naar de lijst");
+            Console.WriteLine($"Titel: {movie.movieDataList[index - 1].titel}\n");
+            Console.WriteLine($"Genre: {ArrToString(movie.movieDataList[index - 1].genre)}\nKijkwijzer: {ArrToString(movie.movieDataList[index - 1].kijkwijzer)}\n");
+            Console.WriteLine($"Regisseur: {movie.movieDataList[index - 1].regisseur}\nActeurs: {ArrToString(movie.movieDataList[index - 1].acteurs)}\n");
+            Console.WriteLine($"samenvatting: {movie.movieDataList[index - 1].samenvatting}\n");
+            Console.WriteLine($"Rating: {movie.movieDataList[index - 1].rating}\nSpeeltijd: {movie.movieDataList[index - 1].speeltijd} minuten");
         }
 
-        public static void MovieList(int page)
+        public static void MovieList(int page = 1)
         {
             // laat een lijst aan films zien //
-            string[] titleList = MovieDetail.GetList(page);
-            for (int i = 0; i < 10; i++)
+            MovieDetail movie = new MovieDetail();
+            Console.WriteLine("vul < of > in om te navigeren tussen de bladzijden, druk het corresponderende nummer in om\nde film informatie te zien.");
+            Console.WriteLine($"page {page}");
+            int x = 0;
+            if (page != 1) x = 10 * (page - 1);
+            for (int i = x; i < x + 10; i++)
             {
-                Console.WriteLine($"{i + 1}.  {titleList[i]}");
+                Console.WriteLine($"{i + 1}.     {movie.movieDataList[i].titel}");
             }
         }
-        static void Main(string[] args)
+
+        public static void CodeActivate()
         {
             int page = 1;
             MovieList(page);
-            while (true)
+            bool retry = true;
+            while (retry)
             {
                 var navigation = Console.ReadLine();
-                if (navigation == "<" || navigation == ">")
+                if (navigation == "<" && page == 1)
+                {
+                    Console.Clear();
+                    MovieList();
+                }
+                else if (navigation == "<" || navigation == ">")
                 {
                     if (navigation == "<" && page > 1)
                     {
@@ -93,29 +133,33 @@ namespace CinaMatrix
                         page--;
                         MovieList(page);
                     }
-                    else if (navigation == ">" && page < 2)
+                    else if (navigation == ">" && page < 5)
                     {
                         Console.Clear();
                         page++;
                         MovieList(page);
                     }
                 }
+                else if (navigation == "x" || navigation == "X")
+                {
+                    retry = false;
+                }
                 else
                 {
-                    // input om proberen te zetten naar int //
-                    try
-                    {
-                        int selection = Convert.ToInt32(navigation);
-                        Console.Clear();
-                        if (selection == 1) DisplayMovie();
-                        else Console.WriteLine("deze film is nog niet beschikbaar");
-                    }
-                    catch
-                    {
-                        Console.WriteLine("dit is geen geldige invulwaarde");
-                    }
+                    int selection = Convert.ToInt32(navigation);
+                    Console.Clear();
+                    MovieDetail movie = new MovieDetail();
+                    DisplayMovie(selection);
                 }
             }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieDetail.CodeActivate();
         }
     }
 }
