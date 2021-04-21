d415ede3b43759d6a1bcb9191b713755cda573fd
MelissaKros <1019740@hr.nl>
2021-04-21 12:29:39 +0200

    display functioneel
    fooddisplay is nu geheel functioneel

--- Team-2-INF1G/Program.cs
+++ Team-2-INF1G/Program.cs
@@ -12,78 +12,122 @@ namespace Project
 {
     public class Eten
     {
-        // declaring struct
-        public struct EtenData {
+        // declaring struct for json data
+        public struct EtenData
+        {
             public string naam;
             public string[] inhoud;
             public double[] prijs;
             public string voedingswaarde;
             public string[] allergenen;
             public string[] tags;
+            public int clicks;
         }
+
         // creates a list of type EtenData
-        public List<EtenData> etenDataList = new List<EtenData>();        
-        
+        public List<EtenData> etenDataList = new List<EtenData>();
+
         [JsonIgnore]
         public string jsonPath;
         [JsonIgnore]
         public string path;
 
         // Constructor
-        public Eten() {
-            // Start van path vinden //
+        public Eten()
+        {
             this.path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\Foods.json"));
             this.jsonPath = File.ReadAllText(path);
             this.etenDataList = JsonConvert.DeserializeObject<List<EtenData>>(jsonPath);
-            // End van path vinden //
         }
 
-        public string ToJSON(){
+        public string ToJSON()
+        {
             return JsonConvert.SerializeObject(this.etenDataList, Formatting.Indented);
         }
-        
-        // menu //
+
+        // fooddisplay
         public void EtenMenu()
         {
-            Console.WriteLine("1. Popcorn zoet - va 2,99\n2. Popcorn zout - va 2,49\n3. Popcorn karamel - va 2,49\n4. M&M's pinda - 3,99\n5. M&M's chocola - 4,49\n6. Chips naturel - va 2,99\n7. Chips paprika - va 2,99\n8. Doritos nacho cheese - 3,99\n9. Haribo goudberen - 3,49\n10. Skittles fruits - 3,99");
-            Console.WriteLine("\nTyp het nummer van wat je wilt bekijken en klik op enter:");
-            
+            Console.WriteLine("Eten Menu:\n1. Popcorn zoet - \t\tva 2,99\n2. Popcorn zout - \t\tva 2,49\n3. Popcorn karamel - \t\tva 2,49\n4. M&M's pinda - \t\t3,99\n5. M&M's chocola - \t\t4,49\n6. Chips naturel - \t\tva 2,99\n7. Chips paprika - \t\tva 2,99\n8. Doritos nacho cheese - \t3,99\n9. Haribo goudberen - \t\t3,49\n10. Skittles fruits - \t\t3,99");
+            Console.WriteLine("\nTyp het nummer van de item die je wilt bekijken en klik op enter:");
+
+            // leest input command van de console
             string input = Console.ReadLine();
-            int num = Convert.ToInt32(input);
             Console.Clear();
-            
-            Console.WriteLine($"naam: {etenDataList[num-1].naam}\ninhoud: {etenDataList[num-1].inhoud}\n.....");
-            Console.WriteLine("Typ 'x' om terug te gaan of '<' om een nieuwe item te selecteren");
 
-            input = Console.ReadLine();
-            if (input == "x" || input == "X") { };
-            if (input == "<") EtenMenu();
-            
+            try // in t geval dat de input te hoog is of niet convertible is tot int
+            {
+                int num = Convert.ToInt32(input);
+                // print naam item
+                Console.WriteLine($"{etenDataList[num - 1].naam}\n");
 
-            /*  if (num == 1) Console.WriteLine("naam: Popcorn zoet \ninhoud: 85g, 180g, 325g \nprijs: 2.99, 4.99, 6.99 \nvoedingswaarde per 100gr:\n   Energie: 1794kj(424kcal)\n   Vet: 5,2g\n   Waarvan verzadigd: 0,7g\n   Koolhydraten: 89g\n   Waarvan suikers: 55g\n   Voedingsvezel: 3,6g\n   Eiwitten: 3,5g\n   Zout: 0g \nAllergenen: veganistisch, glutenvrij, \ntags: zoet");
-            else Console.WriteLine($"Item nummer {num} is nog niet toegevoegd.");*/
+                // print inhoud item
+                string inh = "";
+                for (int i = 0; i < (etenDataList[num - 1].inhoud).Length; i++)
+                {
+                    inh += etenDataList[num - 1].inhoud[i] + ", ";
+                }
+
+                Console.WriteLine("inhoud: " + inh);
+                // print prijs item
+                string pri = "";
+                for (int i = 0; i < (etenDataList[num - 1].prijs).Length; i++)
+                {
+                    pri += etenDataList[num - 1].prijs[i] + ", ";
+                }
+                Console.WriteLine("prijs: " + pri + "\n");
+
+                // print voedingswaarde
+                Console.WriteLine($"{etenDataList[num - 1].voedingswaarde}\n");
+
+                // print allergenen
+                string al = "";
+                for (int i = 0; i < (etenDataList[num - 1].allergenen).Length; i++)
+                {
+                    al += etenDataList[num - 1].allergenen[i] + ", ";
+                }
+                Console.WriteLine("allergenen: " + al);
+
+                // input na de display
+                bool goodInput = false;
+                while (!goodInput)
+                {
+                    Console.WriteLine("\nTyp 'x' om terug te gaan of '>' om een andere item te selecteren.");
+                    input = Console.ReadLine();
+                    Console.Clear();
+                    if (input == "x" || input == "'x'" || input == "X" || input == "'X'") { goodInput = true; break; }
+                    else if (input == ">" || input == "'>'") { EtenMenu(); goodInput = true; break; }
+                    else Console.WriteLine("Er is een onjuist command ingevuld, probeer het nog eens.");
+                }
+            }
+
+            catch (Exception e)
+            { // input is niet convertible naar int of input is te hoog nummer
+                Console.Clear();
+                Console.WriteLine("De input is niet juist, probeer het nogeens\n");
+                EtenMenu();
+            }
+            Console.Clear();
+
         }
 
-        // food info //
-        //public void etenInfo(){
-        //Console.WriteLine(Foods.json[num-1][naam]);
-        //Console.WriteLine(Foods.json[num-1][inhoud]);
-        //Console.WriteLine(Foods.json[num-1][prijs]);
-        //Console.WriteLine(Foods.json[num-1][voedingswaarde]);
-        //Console.WriteLine(Foods.json[num-1][allergenen]);
-        //Console.WriteLine(Foods.json[num-1][tags]);
-        //Console.WriteLine("Toets x om terug te gaan");
-        //if (Console.ReadLine() == 'x') call menu()
-        //}
+        /* public void UpdateClick(int n)
+         {
+             int currClick = etenDataList[n - 1].clicks;
+             currClick++;
+             etenDataList[n - 1].clicks = currClick;
+
+             // write to the JSON file (updates the file)
+             System.IO.File.WriteAllText(this.path, ToJSON());
+         }*/
     }
+
     class Program
     {
         static void Main(string[] args)
         {
-
             Eten eten = new Eten();
             eten.EtenMenu();
-
         }
     }
 }    
