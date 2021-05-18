926910813f3d5b4ba9d1a82260e6a90e7c696e0e
MelissaKros <1019740@hr.nl>
2021-05-18 09:57:16 +0200

    niewe class
    nieuwe class gemaakt voor plattegrond

---
+++ Team-2-INF1G/Classes/Plattegrond.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Team_2_INF1G.Classes
+{
+    class Plattegrond
+    {
+        public void PrintPlattegrond() {
+            Console.WriteLine();
+        }
+    }
+}
